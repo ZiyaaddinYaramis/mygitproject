988d128fdb228fa5a1f8df360c1a62d5d82fb7aa
ZiyaaddinYaramis <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2021-04-04 19:12:56 +0300

    Update MyGitClass.java

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	System.out.println("Version 1, 2, 3.commit");
 
 	
-	/*
+	/* pc'den githup'a
 	 * 1.Javadaki proje icine girilmesi lazim
 	 * 2.git init (****) ilk sefer yapilmasi yeterlidir
 	 * 3.git status
@@ -16,11 +16,30 @@ public static void main(String[] args) {
 	 * 5.git status
 	 * 6.Github'da repostory olusturulur
 	 * 7.git commit -m "mesaj buraya yazilir"
+	 * 8.git remote add origin kismi github'dan kopyalaniyor cmd ye atiliyor ??????
+	 * 9.git push -u master
+	 * 
+	 * 
+	 * github'dan pc'ye
+	 * 
+	 * 1.git fetch
+	 * 2.git merge
+	 * 3.git pull
+	 * 
+	 * 
+	 * 
+	 * Cesitli komutlar:
+	 * 1.git log = version'lari gösterir
+	 * 
+	 * 
+	 * 
+	 * 
 	 */
 
 	System.out.println("uzak sunucudaki kodum");
 	System.out.println("uzak sunucudaki kodum1");
 	System.out.println("pratic");
+	System.out.println("42. satir")
 		
 
 }

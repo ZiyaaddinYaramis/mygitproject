f9cdad81f96a010c88b93e3fd62b6e179bb2c2ce
Ziyaaddin <ziyaaddinyaramis@gmail.com>
2021-04-04 18:29:25 +0300

    version

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -6,5 +6,15 @@ public static void main(String[] args) {
 	System.out.println("Version 1");
 	System.out.println("Version 1, 2.commit");
 	System.out.println("Version 1, 2, 3.commit");
+	
+	/*
+	 * 1.Javadaki proje icine girilmesi lazim
+	 * 2.git init (****) ilk sefer yapilmasi yeterlidir
+	 * 3.git status
+	 * 4.git add
+	 * 5.git status
+	 * 6.Github'da repostory olusturulur
+	 * 7.git commit -m "mesaj buraya yazilir"
+	 */
 }
 }

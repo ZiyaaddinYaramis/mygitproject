e07838a3a6e0972f7e26ce2f57288947632a2133
ZiyaaddinYaramis <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2021-04-04 18:13:59 +0300

    Update MyGitClass.java
    Buraya not ekleyebilirsiniz

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Version 1");
 	System.out.println("Version 1, 2.commit");
 	System.out.println("Version 1, 2, 3.commit");
+	System.out.println("uzak sunucudaki kodum")
 }
 }

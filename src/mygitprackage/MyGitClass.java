2c16549bee56b0f13862c68561cad80082ffc127
ZiyaaddinYaramis <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2021-04-04 18:30:52 +0300

    Update MyGitClass.java

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("Version 1");
 	System.out.println("Version 1, 2.commit");
 	System.out.println("Version 1, 2, 3.commit");
-<<<<<<< HEAD
+
 	
 	/*
 	 * 1.Javadaki proje icine girilmesi lazim
@@ -17,8 +17,8 @@ public static void main(String[] args) {
 	 * 6.Github'da repostory olusturulur
 	 * 7.git commit -m "mesaj buraya yazilir"
 	 */
-=======
+
 	System.out.println("uzak sunucudaki kodum")
->>>>>>> e07838a3a6e0972f7e26ce2f57288947632a2133
+
 }
 }

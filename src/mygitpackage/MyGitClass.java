5b47f0a9b365538f3b7c9492b0350cc0220e9195
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2021-04-14 03:20:48 +0300

    Branch son deneme

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,11 +6,9 @@ public static void main(String[] args) {
 	System.out.println("Version 1");
 	System.out.println("Version 1, 2.commit");
 	System.out.println("Version 1, 2, 3.commit");
-	
 	System.out.println("Ziya Branchinde yapilan kodlar");
-	
 	System.out.println("Ziya branchinde yazdim");
-
+	System.out.println("Ziya branchinde son deneme");
 		
 	/* 
 	  
